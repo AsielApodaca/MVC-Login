5e902b059b3c09b093b5fb77e11ea9aae0555233
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-09-25 16:04:36 -0700

    metodos de modelo

--- PruebaMVC/src/main/java/model/LoginModel.java
+++ PruebaMVC/src/main/java/model/LoginModel.java
@@ -10,8 +10,28 @@ package model;
  */
 public class LoginModel {
     
+    private String user;
+    private String password;
+    
     public LoginModel() {
-        
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
     
 }
